2ceaa1f3a229bf6777e2c16e471a177ad5b16f68
SilkRoadRunner <55788243+SilkRoadRunner@users.noreply.github.com>
2020-08-31 11:38:09 +0200

    add logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import logo from '../Assets/logo.png';
+import styles from './LandingPage.module.css';
+
 export function LandingPage() {
   return (
     <div>
        <TopNav/>
+       <img src={logo} className={styles.logo} alt='logo'/>
     </div>
   );
 }
